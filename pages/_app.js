49b51619cdbef3dedfb2f78a670b80cb76437c0d
Anish Agnihotri <contact@anishagnihotri.com>
2021-05-09 04:09:43 -0400

    Functional overview

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
-import "../styles/globals.scss";
-import StateProvider from "@state/index";
+// Utilities
+import StateProvider from "@state/index"; // State provider wrapper
+
+// Styles
+import "../styles/globals.scss"; // Global styles
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"; // Spinners
 
 export default function Unify({ Component, pageProps }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import axios from "axios"; // Requests
+import Link from "next/link"; // Routing: Links
 import Card from "@components/Card"; // Component: Card
-import { useRouter } from "next/router"; // Routing
+import { useRouter } from "next/router"; // Routing: Router
 import Layout from "@components/Layout"; // Component: Layout
+import Loader from "react-loader-spinner"; // Loaders
+import { useState, useEffect } from "react"; // State management
 import Breadcrumb from "@components/Breadcrumb"; // Component: Breadcrumb
-import { useState, useEffect } from "react";
 import styles from "@styles/pages/Home.module.scss"; // Component styles
 
 export default function Home() {
@@ -20,7 +22,11 @@ export default function Home() {
     router.push("/create");
   };
 
+  /**
+   * Fetch proposals from api endpoint
+   */
   const fetchProposals = async () => {
+    // Collect proposal data
     const response = await axios.get("/api/proposals");
     const data = response.data;
 
@@ -46,16 +52,34 @@ export default function Home() {
         }}
       >
         {loading ? (
-          <div className={styles.home__loading}>
-            <p>Loading...</p>
+          <div className="card__padding">
+            <center>
+              <Loader type="Oval" color="#e7347a" height={50} width={50} />
+            </center>
           </div>
         ) : proposals.length < 1 ? (
-          <div className={styles.home__loading}>
-            <p>No Proposals</p>
+          <div className="card__padding">
+            <div className={styles.home__empty}>
+              <h3>No Autonomous Proposals Found</h3>
+            </div>
           </div>
         ) : (
           <div className={styles.home__loading}>
-            <p>Some proposals</p>
+            {proposals.map((proposal, i) => {
+              return (
+                <Link href={`/proposal/${proposal.contract}`} key={i}>
+                  <a className={styles.home__proposal}>
+                    <div>
+                      <h4>{proposal.title}</h4>
+                      <span>{proposal.timestamp}</span>
+                    </div>
+                    <div>
+                      <span>Status</span>
+                    </div>
+                  </a>
+                </Link>
+              );
+            })}
           </div>
         )}
       </Card>
