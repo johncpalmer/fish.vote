544e1c82cdb8d0fa122d713bdff3c7f9b7f3bff4
Kenneth Ashley <ken.u.diggitt@gmail.com>
2021-11-15 15:54:34 -0500

    update favicon

--- components/Layout.js
+++ components/Layout.js
@@ -88,7 +88,7 @@ function Meta({ proposal }) {
       />
 
       {/* Favicon */}
-      <link rel="shortcut icon" href="/favicon.ico" />
+      <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🔥</text></svg>"></link>
 
       {/* Fortmatic verification */}
       <meta name="fortmatic-site-verification" content="YB1r9idOuJBbDdbk" />
